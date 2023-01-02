2dbf1cfe8311badb0f9b55dc0566aa57020133e6
Kucci <kucciyeager@gmail.com>
2023-01-02 13:56:02 +0530

    Added responsiveness to about and skills section

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,13 +2,6 @@ var openBtn = document.querySelector("#navbar-open")
 var closeBtn = document.querySelector("#navbar-close")
 var sideBar = document.querySelector(".navbar")
 
-if(screen.width <= 768) {
-    openBtn.style.display = "initial";
-    closeBtn.style.display = "initial"
-    sideBar.style.top = "-100vh"
-    sideBar.style.width = "100vw"
-}
-
 openBtn.onclick = () => {
     sideBar.style.top = "0"
 }
