7c3b4e8c61ca991da6b5efbd1784d6c3347ae03b
Kucci <kucciyeager@gmail.com>
2023-01-04 17:16:03 +0530

    Tweaked some things in navbar and added better responsive experience

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,8 @@
-var hamburger = document.getElementById("hamburger")
-var sideBar = document.getElementById("navbar")
+let hamburger = document.getElementById("hamburger")
+let sideBar = document.getElementById("navbar")
+let links = document.querySelectorAll(".nav-link")
 
-hamburger.onclick = () => {
+function hamburgerClick() {
     sideBar.classList.toggle("open")
     if (hamburger.innerText == "menu") {
         hamburger.innerText = "close";
@@ -10,3 +11,13 @@ hamburger.onclick = () => {
         hamburger.innerText = "menu";
     }
 }
+
+hamburger.onclick = () => { 
+    hamburgerClick()
+}
+
+links.forEach(element => {
+    element.addEventListener('click', event => {
+        hamburgerClick()
+    })
+});
