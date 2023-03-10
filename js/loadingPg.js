f922fbdd7e336e4cc7f6ca647eed9b7c38df685b
Kucci <kucciyeager@gmail.com>
2023-03-10 15:32:53 +0530

    Added structure of loading page

---
+++ js/loadingPg.js
@@ -0,0 +1,6 @@
+let loadingPg = document.getElementById("loadingPg")
+let body = document.getElementById("body")
+function whenLoaded() {
+    loadingPg.style.display = "none";
+    body.style.overflow = "initial";
+}
